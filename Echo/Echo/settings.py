746e657c1dbc361977b6506853a2383b9d82cee1
MysticalFlame3 <suryaraj.9274@gmail.com>
2025-03-08 04:24:59 +0530

    deployment

--- Echo/Echo/settings.py
+++ Echo/Echo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure--&_ji+(v_hude!ip^-g_u!mcqi0#pwc=g1eks=6y+k%=!1p--u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -76,12 +76,12 @@ WSGI_APPLICATION = 'Echo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
