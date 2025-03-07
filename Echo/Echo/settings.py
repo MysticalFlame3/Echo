cf80bb6c1eb072fd67bb3f30368645549813d3da
MysticalFlame3 <suryaraj.9274@gmail.com>
2025-03-08 02:41:54 +0530

    deployment

--- Echo/Echo/settings.py
+++ Echo/Echo/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--&_ji+(v_hude!ip^-g_u!mcqi0#pwc=g1eks=6y+k%=!1p--u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
